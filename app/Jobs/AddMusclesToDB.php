2bf08db548e4841415c6d03397d8f163db18ec27
Emil Johansson <emij@student.chalmers.se>
2015-07-06 15:11:38 +0200

    added inserts for all muscles

--- app/Jobs/AddMusclesToDB.php
+++ app/Jobs/AddMusclesToDB.php
@@ -31,7 +31,145 @@ class AddMusclesToDB extends Job implements SelfHandling, ShouldQueue
     public function handle()
     {
         DB::table('muscles')->insert(
-            ['name' => '', 'latin_name' => '']
+            ['name' => 'Adductors']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Biceps Brachii']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Brachialis']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Brachioradialis']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Deltoid Anterior']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Deltoid Lateral']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Deltoid Posterior']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Deep Hip External Rotators']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Erector Spinae']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Gastrocnemius']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Gluteus Maximus']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Gluteus Medius']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Gluteus Minimus']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Gracilis']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Hamstrings']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Iliopsoas']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Infraspinatus']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Latissimus Dorsi']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Levator Scapulae']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Obliques']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Pectineous']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Pectoralis Major Clavicular Head']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Pectoralis Major Sternal Head']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Pectoralis Minor']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Popliteus']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Quadratus Lumborum']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Quadriceps']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Rectus Abdominis']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Rhomboids']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Sartorius']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Serratus Anterior']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Soleus']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Splenius']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Sternocleidomastoid']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Subscapularis']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Supraspinatus']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Tensor Fasciae Latae']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Teres Major']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Teres Minor']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Tibialis Anterior']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Transverse Abdominus']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Trapezius Lower Fibers']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Trapezius Middle Fibers']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Trapezius Upper Fibers']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Triceps Brachii']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Wrist Extensors']
+        );
+        DB::table('muscles')->insert(
+            ['name' => 'Wrist Flexors']
         );
     }
 }
