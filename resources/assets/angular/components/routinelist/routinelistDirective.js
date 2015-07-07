f325403ae042d11ee7a01ec896d3d25ad379309b
Krabban <anderssongabriel665@gmail.com>
2015-07-07 13:25:45 +0200

    Fixed a mistype in muscleService.js

---
+++ resources/assets/angular/components/routinelist/routinelistDirective.js
@@ -0,0 +1,43 @@
+gains.directive('routinelist', function() {
+  return {
+    templateUrl: "components/routinelist/routinelistView.html"
+  };
+});
+/*
+
+gains.directive("routineList", function() {
+    $scope.exercises = function(){ return [
+        {"name": "Bench-Press",
+         "muscles": [
+            {
+                "name": "boobie",
+                "intensity": 100
+            }
+         ],
+         "description": "Good choice."
+         },
+        {"name": "Bench-Press2",
+         "muscles": [
+            {
+                "name": "boobie",
+                "intensity": 100
+            }
+         ],
+         "description": "Good choice."
+         },
+        {"name": "Bench-Press1",
+         "muscles": [
+            {
+                "name": "boobie",
+                "intensity": 100
+            }
+         ],
+         "description": "Good choice."
+         }
+      ];};
+
+      $scope.onDropComplete = function(index,data,event){
+        return true;
+      };
+              
+});*/
