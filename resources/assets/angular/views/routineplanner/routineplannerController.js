a481f78e6a34182c4c20589e8e8947c819ccb245
Krabban <anderssongabriel665@gmail.com>
2015-07-07 17:06:21 +0200

    Dragging between two workout routines now works. but not within...

--- resources/assets/angular/views/routineplanner/routineplannerController.js
+++ resources/assets/angular/views/routineplanner/routineplannerController.js
@@ -1,4 +1,69 @@
 
-gains.controller('RoutinePlannerController', function() {
+gains.controller('RoutinePlannerController', function($scope) {
+  //temporary
+  $scope.routines = [{
+  	"exercises": [
+            {
+                "name": "Press",
+                "muscles": [
+                    {
+                        "name": "boobie",
+                        "intensity": 100
+                    }
+                ],
+             "description": "Good choice."
+            },
+            {"name": "Bench-Press2",
+             "muscles": [
+                {
+                    "name": "boobie",
+                    "intensity": 100
+                }
+             ],
+             "description": "Good choice."
+             },
+            {"name": "Bench-Press1",
+             "muscles": [
+                {
+                    "name": "boobie",
+                    "intensity": 100
+                }
+             ],
+             "description": "Good choice."
+             }]
+  },
+  {
+  	"exercises": [
+    {
+        "name": "pull",
+        "muscles": [
+            {
+                "name": "backie",
+                "intensity": 100
+            }
+        ],
+     "description": "Bad choice."
+    },
+    {"name": "Bench-fail",
+     "muscles": [
+        {
+            "name": "boobie",
+            "intensity": 100
+        }
+     ],
+     "description": "Good choice."
+     },
+    {"name": "Bench-Pull",
+     "muscles": [
+        {
+            "name": "Backie",
+            "intensity": 10
+        }
+     ],
+     "description": "Good choice."
+     }]
+  }];
+  $scope.testing = "hejsvejs";
+
   // Controller code here.
 });
