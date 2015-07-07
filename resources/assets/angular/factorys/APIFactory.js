a861605539910f9be2761d78890ef915722a03ab
Jonathan Thunberg <Vikingaskor@hotmail.com>
2015-07-07 16:44:20 +0200

    database factory

---
+++ resources/assets/angular/factorys/APIFactory.js
@@ -0,0 +1,29 @@
+gains.factory('mpApiFactory', ['$http', function($http) {
+
+    'use strict';
+    var apiRoute = "http://localhost:8000";
+
+    return {
+        getMuscles: function () {
+            return $http.get(apiRoute + '/getMuscles').
+                success(function (data, status, headers, config) {
+                    return data;
+                }).
+                error(function (data, status, headers, config) {
+                    // log error
+                    return false;
+                });
+        },
+        getExersise: function () {
+            return $http.get(apiRoute + '/exercise').
+            success(function (data, status, headers, config) {
+                return data;
+            }).
+                error(function (data, status, headers, config) {
+                    // log error
+                    return false;
+                });
+        }
+    };
+
+}]);
