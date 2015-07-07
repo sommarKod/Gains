edea8fadc0a5a9a5faa01d6bc13d69a3a61ec9fb
Heff <heff@heff.se>
2015-07-07 17:10:30 +0200

    Color mapping added, need tweaks.

--- resources/assets/angular/components/anatomy/anatomyDirective.js
+++ resources/assets/angular/components/anatomy/anatomyDirective.js
@@ -7,6 +7,7 @@ angular.module('gains').directive('anatomyimage', ['$compile', function ($compil
             angular.forEach(regions, function (path, key) {
                 var regionElement = angular.element(path);
                 regionElement.attr("muscleregion", "");
+                regionElement.attr("dummy-data", "dummyData");
                 $compile(regionElement)(scope);
             });
         }
@@ -16,15 +17,44 @@ angular.module('gains').directive('anatomyimage', ['$compile', function ($compil
 angular.module('gains').directive('muscleregion', ['$compile', function ($compile) {
     return {
         restrict: 'A',
-        scope: true,
+        scope: {
+            dummyData: "="
+        },
         link: function (scope, element, attrs) {
             scope.elementId = element.attr("id");
             scope.regionClick = function () {
-                alert(scope.elementId);
+                alert(scope.elementId + ":" + scope.dummyData[scope.elementId].value);
             };
             element.attr("ng-click", "regionClick()");
+            element.attr("ng-attr-fill", "{{dummyData[elementId].value | map_intensity_colour}}");
             element.removeAttr("muscleregion");
             $compile(element)(scope);
         }
+    };
+}]);
+
+angular.module('gains').filter('map_intensity_colour', [function () {
+    return function (input) {
+        var r = 255;
+        var b = 255;
+        var g = 255;
+        if (input < 25) { // 00 00 FF -> 00 FF FF
+            r = 0;
+            b = Math.floor(input * 4 * 2.55); // [0->25] * 4 * 2.55 = 0 -> 255
+        } else if (input < 50) { // 00 FF FF -> 00 FF 00
+            r = 0;
+            g = 255 - Math.floor((input - 25) * 4 * 2.55); // ([25->50] - 25) * 4 * 2.55 = 0 -> 255
+        } else if (input < 75) { // 00 FF 00 -> FF FF 00
+            g = 0;
+            r = Math.floor((input - 50) * 4 * 2.55); // ([50->75] - 50) * 4 * 2.55 = 0 -> 255
+        } else { // FF FF 00 -> FF 00 00
+            g = 0;
+            b = 255 - Math.floor((input - 75) * 4 * 2.55); // ([25->75] - 25) * 4 * 2.55 = 0 -> 255
+        }
+        console.log(input);
+        console.log(r);
+        console.log(g);
+        console.log(b);
+        return "rgba(" + r + "," + g + "," + b + ",1)";
     };
 }]);
--- resources/assets/angular/views/routineplanner/routineplannerController.js
+++ resources/assets/angular/views/routineplanner/routineplannerController.js
@@ -4,7 +4,7 @@ gains.controller('RoutinePlannerController', ['$scope', function ($scope) {
         $scope.createDummyData = function () {
             var temp = {};
             angular.forEach(muscleRegions, function (region, key) {
-                temp[region] = {value: Math.random()};
+                temp[region] = {value: Math.random() * 100};
             });
             $scope.dummyData = temp;
         };
