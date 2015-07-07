60caa1f048371871278d259ec242625cc0c956b1
Krabban <anderssongabriel665@gmail.com>
2015-07-07 14:07:14 +0200

    Excercise item converted to being a directive

---
+++ resources/assets/angular/components/routinelist/exerciseitem/exerciseitemDirective.js
@@ -0,0 +1,8 @@
+gains.directive('exerciseitem', function() {
+  return {
+    templateUrl: "components/routinelist/exerciseitem/exerciseitemView.html",
+    link: function(scope, element, attr){
+        
+    }
+  };
+});
--- resources/assets/angular/components/routinelist/routinelistDirective.js
+++ resources/assets/angular/components/routinelist/routinelistDirective.js
@@ -1,43 +1,37 @@
 gains.directive('routinelist', function() {
   return {
-    templateUrl: "components/routinelist/routinelistView.html"
+    templateUrl: "components/routinelist/routinelistView.html",
+    link: function(scope, element, attr){
+        scope.exercises = [
+            {
+                "name": "Press",
+                "muscles": [
+                    {
+                        "name": "boobie",
+                        "intensity": 100
+                    }
+                ],
+             "description": "Good choice."
+            },
+            {"name": "Bench-Press2",
+             "muscles": [
+                {
+                    "name": "boobie",
+                    "intensity": 100
+                }
+             ],
+             "description": "Good choice."
+             },
+            {"name": "Bench-Press1",
+             "muscles": [
+                {
+                    "name": "boobie",
+                    "intensity": 100
+                }
+             ],
+             "description": "Good choice."
+             }];
+        
+    }
   };
 });
-/*
-
-gains.directive("routineList", function() {
-    $scope.exercises = function(){ return [
-        {"name": "Bench-Press",
-         "muscles": [
-            {
-                "name": "boobie",
-                "intensity": 100
-            }
-         ],
-         "description": "Good choice."
-         },
-        {"name": "Bench-Press2",
-         "muscles": [
-            {
-                "name": "boobie",
-                "intensity": 100
-            }
-         ],
-         "description": "Good choice."
-         },
-        {"name": "Bench-Press1",
-         "muscles": [
-            {
-                "name": "boobie",
-                "intensity": 100
-            }
-         ],
-         "description": "Good choice."
-         }
-      ];};
-
-      $scope.onDropComplete = function(index,data,event){
-        return true;
-      };
-              
-});*/
