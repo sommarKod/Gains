30b83261ac3f6c0aa8a332f7137d1b788ab9ee0d
Erik Axelsson <erikaxelsson1@gmail.com>
2015-07-07 16:47:23 +0200

    fix getWorkoutPlan and getWorkoutPlans

--- resources/assets/angular/factorys/APIFactory.js
+++ resources/assets/angular/factorys/APIFactory.js
@@ -23,6 +23,26 @@ gains.factory('mpApiFactory', ['$http', function($http) {
                     // log error
                     return false;
                 });
+        },
+        getWorkoutPlan: function (id) {
+            return $http.get(apiRoute + '/workoutPlan/'+id).
+                success(function (data, status, headers, config) {
+                    return data;
+                }).
+                error(function (data, status, headers, config) {
+                    // log error
+                    return false;
+                });
+        },
+        getWorkoutPlans: function () {
+            return $http.get(apiRoute + '/workoutPlan').
+                success(function (data, status, headers, config) {
+                    return data;
+                }).
+                error(function (data, status, headers, config) {
+                    // log error
+                    return false;
+                });
         }
     };
 
