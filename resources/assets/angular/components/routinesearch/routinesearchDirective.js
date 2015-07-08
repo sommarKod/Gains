fe958297b7b75b029be43b39a9cdd01fba6ea86f
Krabban <anderssongabriel665@gmail.com>
2015-07-08 14:38:42 +0200

    Added the ng-sortable to search view

--- resources/assets/angular/components/routinesearch/routinesearchDirective.js
+++ resources/assets/angular/components/routinesearch/routinesearchDirective.js
@@ -5,6 +5,22 @@ gains.directive('routinesearch',['ApiFactory',function (mpApiFactory) {
        mpApiFactory.getExersise().success(function(data){
          scope.works = data;
        });
+
+       scope.dragControlListeners = {
+		    accept: function (sourceItemHandleScope, destSortableScope) {
+	            //console.log("accept "+sourceItemHandleScope.itemScope.sortableScope.$id === destSortableScope.$id);
+            	return sourceItemHandleScope.itemScope.sortableScope.$id === destSortableScope.$id;
+		    },
+		    itemMoved: function (event) {
+		    	console.log(event.dest.sortableScope.$parent.column.name);
+		    	event.source.itemScope.modelValue.status = event.dest.sortableScope.$parent.column.name;
+		    	//Do what you want
+			},
+		    orderChanged: function(event) {
+		    	//Do what you want
+			},
+		    containment: '#search'//optional param.
+		};
     }
   };
 }]);
