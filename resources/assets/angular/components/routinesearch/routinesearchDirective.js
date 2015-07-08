5da665e0ed35f8ad6f1358168f49cf5a6a8938d0
Jonathan Thunberg <Vikingaskor@hotmail.com>
2015-07-08 14:36:33 +0200

    Searchbox now working

--- resources/assets/angular/components/routinesearch/routinesearchDirective.js
+++ resources/assets/angular/components/routinesearch/routinesearchDirective.js
@@ -5,6 +5,28 @@ gains.directive('routinesearch',['ApiFactory',function (mpApiFactory) {
        mpApiFactory.getExersise().success(function(data){
          scope.works = data;
        });
+
+       scope.colapse= function(index) {
+       //    var thi = angular.element(this);
+     //      console.log(thi[0].class =='.routineexe');
+           var routineexe = element[0].querySelectorAll('.exercisesearch');
+           var panels = [];
+           angular.forEach(routineexe,function(path,key){
+             var exe = angular.element(path);
+             var exercises = exe[0].querySelectorAll('.panel-collapse');
+             panels.push(exercises);
+           });
+
+           angular.forEach(panels,function(path,key){
+                 var panel = angular.element(path);
+                 console.log(panel);
+                 panel.collapse('hide');
+           });
+
+           var whospan =  angular.element(panels[index]);
+           console.log(whospan);
+           whospan.collapse('show');
+         };
     }
   };
 }]);
