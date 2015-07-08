5ab02f3b3a9f31f4af3185d8fab2a6da1e477a2f
Emil Johansson <emij@student.chalmers.se>
2015-07-08 16:06:14 +0200

    added update to workoutcontroller

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -73,13 +73,21 @@ class WorkoutController extends Controller
      */
     public function update($id)
     {
+        $workout = Workout::find($id);
 
+        $workout->removeAllExercises();
+        addEx($workout);
+
+        $workout->save();
+    }
+    private function addEx($workout){
+        $workout->addExercises(Input::all());
     }
 
     public function addExercises($id)
     {
         $workout = Workout::find($id);
-        $workout->addExercises(Input::all());
+        addEx($workout);
     }
 
     /**
