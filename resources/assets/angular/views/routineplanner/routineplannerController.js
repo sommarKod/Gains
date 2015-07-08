aef71acc1469e874b53ba35be809721c77021cd3
Erik Axelsson <erikaxelsson1@gmail.com>
2015-07-08 10:53:20 +0200

    add underscore

--- resources/assets/angular/views/routineplanner/routineplannerController.js
+++ resources/assets/angular/views/routineplanner/routineplannerController.js
@@ -2,9 +2,10 @@
 gains.controller('RoutinePlannerController',['$location','ApiFactory','$scope',function($location,APIFactory,$scope) {
     var searchObject = $location.search();
     $scope.board=searchObject.board;
-    APIFactory.getWorkoutPlans($scope.board).success(
+    APIFactory.getWorkoutPlan($scope.board).success(
         function(data){
-            console.log(data);
+            //console.log(data);
+            
         }
     );
 
