babe16d459c25f6044e8abb4e81c224a52d3dd5b
Emil Johansson <emij@student.chalmers.se>
2015-08-03 10:38:05 +0200

    changed search stuff

--- resources/assets/angular/components/routinesearch/muscleGroupListDirective.js
+++ resources/assets/angular/components/routinesearch/muscleGroupListDirective.js
@@ -2,8 +2,18 @@ gains.directive('musclegrouplist',[function () {
     return {
         templateUrl: "components/routinesearch/muscleGroupListView.html",
         link: function(scope, element, attr){
-            scope.muscleGroupChoosen = function () {
-                alert("hej");
+            scope.muscleGroupChoosen = function (event, musclegroup) {
+                var target = $(event.target);
+
+
+                if(target.hasClass("pushed-musclegroup")){
+                    target.removeClass("pushed-musclegroup");
+                    scope.selectedMuscleGroup = undefined;
+                } else {
+                    $(".pushed-musclegroup").removeClass("pushed-musclegroup");
+                    $(event.target).addClass("pushed-musclegroup");
+                    scope.selectedMuscleGroup = musclegroup;
+                }
             };
           }
     };
